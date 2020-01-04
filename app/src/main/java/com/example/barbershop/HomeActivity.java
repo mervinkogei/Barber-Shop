3abf013b5a5c2c22d43b4ee4d35eeb69932a4e86
mervinkogei <vincent.kogei@yahoo.com>
2020-01-04 09:07:06 +0300

    Add libraries to slide banner images and to load images

--- app/src/main/java/com/example/barbershop/HomeActivity.java
+++ app/src/main/java/com/example/barbershop/HomeActivity.java
@@ -1,24 +1,34 @@
 package com.example.barbershop;
 
 import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import android.app.AlertDialog;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.barbershop.Common.Common;
 import com.example.barbershop.Fragments.BookingFragment;
 import com.example.barbershop.Fragments.HomeFragment;
+import com.example.barbershop.Models.User;
 import com.facebook.accountkit.Account;
 import com.facebook.accountkit.AccountKit;
 import com.facebook.accountkit.AccountKitCallback;
 import com.facebook.accountkit.AccountKitError;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -26,6 +36,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import dmax.dialog.SpotsDialog;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -36,6 +47,8 @@ public class HomeActivity extends AppCompatActivity {
 
     CollectionReference userRef;
 
+    AlertDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,62 +57,115 @@ public class HomeActivity extends AppCompatActivity {
 
         //Initialization
         userRef = FirebaseFirestore.getInstance().collection("user");
+        dialog = new SpotsDialog.Builder().setContext(this).setCancelable(false).build();
 
-        if (getIntent() !=null){
-            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN,false);
-            if (isLogin){
+        if (getIntent() != null) {
+            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN, false);
+            if (isLogin) {
+                dialog.show();
                 AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
                     @Override
                     public void onSuccess(Account account) {
-                        if (account != null){
+                        if (account != null) {
                             DocumentReference currentUser = userRef.document(account.getPhoneNumber().toString());
                             currentUser.get()
                                     .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                                         @Override
                                         public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                           if (task.isSuccessful()){
-                                               DocumentSnapshot userSnapshot = task.getResult();
-                                               if (!userSnapshot.exists())
-                                                   showUpdateDialog(account.getPhoneNumber().toString());
-                                           }
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot userSnapshot = task.getResult();
+                                                if (!userSnapshot.exists())
+                                                    showUpdateDialog(account.getPhoneNumber().toString());
+                                            }
+                                            if (dialog.isShowing()){
+                                                dialog.dismiss();
+                                            }
                                         }
                                     });
                         }
-                        //view
-                        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                            Fragment fragment = null;
-                            @Override
-                            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                                if (menuItem.getItemId()== R.id.action_home)
-                                    fragment = new HomeFragment();
-                                else if (menuItem.getItemId() == R.id.action_cart)
-                                    fragment = new BookingFragment();
 
-                                return loadFragment(fragment);
-                            }
-                        });
 
                     }
 
                     @Override
                     public void onError(AccountKitError accountKitError) {
-                        Toast.makeText(HomeActivity.this, ""+accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(HomeActivity.this, "" + accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         }
+        //view
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.action_home)
+                    fragment = new HomeFragment();
+                else if (menuItem.getItemId() == R.id.action_cart)
+                    fragment = new BookingFragment();
+
+                return loadFragment(fragment);
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
     }
 
     private boolean loadFragment(Fragment fragment) {
-        if (fragment != null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment)
-            .commit();
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit();
             return true;
         }
-        return  false;
+        return false;
     }
 
+
     private void showUpdateDialog(String phoneNumber) {
+
+        //Initialize Dialog
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setTitle("One More Step");
+        bottomSheetDialog.setCanceledOnTouchOutside(false);
+        bottomSheetDialog.setCancelable(false);
+        View sheetView = getLayoutInflater().inflate(R.layout.layout_update_information, null);
+
+        Button btn_update = (Button) sheetView.findViewById(R.id.btn_update);
+        TextInputEditText edt_name = (TextInputEditText) sheetView.findViewById(R.id.edt_name);
+        TextInputEditText edt_address = (TextInputEditText) sheetView.findViewById(R.id.edt_address);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                if (!dialog.isShowing()){
+                    dialog.show();
+                }
+                User user = new User(edt_name.getText().toString(),
+                        edt_address.getText().toString(), phoneNumber);
+                userRef.document(phoneNumber)
+                        .set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                if (dialog.isShowing())
+                                    dialog.dismiss();
+                                bottomSheetDialog.dismiss();
+                                Toast.makeText(HomeActivity.this, "Thank You", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        bottomSheetDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+            }
+        });
+        bottomSheetDialog.setContentView(sheetView);
+        bottomSheetDialog.show();
+
     }
 }
+
