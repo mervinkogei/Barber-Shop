35a509e2e7ef0b5cae8f7132a4e8441ed4de5b52
mervinkogei <vincent.kogei@yahoo.com>
2020-01-04 10:31:52 +0300

    Add Interface package and interface class to load images from firebase

---
+++ app/src/main/java/com/example/barbershop/Adapter/HomeSliderAdapter.java
@@ -0,0 +1,23 @@
+package com.example.barbershop.Adapter;
+
+import com.example.barbershop.Models.Banner;
+
+import java.util.List;
+
+import ss.com.bannerslider.adapters.SliderAdapter;
+import ss.com.bannerslider.viewholder.ImageSlideViewHolder;
+
+public class HomeSliderAdapter extends SliderAdapter {
+
+    List<Banner> bannerList;
+
+    @Override
+    public int getItemCount() {
+        return bannerList.size();
+    }
+
+    @Override
+    public void onBindImageSlide(int position, ImageSlideViewHolder imageSlideViewHolder) {
+        imageSlideViewHolder.bindImageSlide(bannerList.get(position).getImage());
+    }
+}
