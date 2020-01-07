19261bdc09b67b31f3e0a9855e0808e985685c16
mervinkogei <vincent.kogei@yahoo.com>
2020-01-07 15:15:31 +0300

    Update the MySalonAdapter with CardView

--- app/src/main/java/com/example/barbershop/Adapter/MySalonAdapter.java
+++ app/src/main/java/com/example/barbershop/Adapter/MySalonAdapter.java
@@ -7,33 +7,56 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.barbershop.Interface.IRecyclerItemSelectedListener;
 import com.example.barbershop.Models.Salon;
 import com.example.barbershop.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MySalonAdapter extends RecyclerView.Adapter<MySalonAdapter.MyViewHolder> {
 
     Context context;
     List<Salon> salonList;
+    List<CardView> cardViewList;
 
     public MySalonAdapter(Context context, List<Salon> salonList) {
         this.context = context;
         this.salonList = salonList;
+        cardViewList = new ArrayList<>();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView txt_salon_name,txt_salon_address;
+        CardView card_salon;
+
+        IRecyclerItemSelectedListener iRecyclerItemSelectedListener;
+
+        public IRecyclerItemSelectedListener getiRecyclerItemSelectedListener() {
+            return iRecyclerItemSelectedListener;
+        }
+
+        public void setiRecyclerItemSelectedListener(IRecyclerItemSelectedListener iRecyclerItemSelectedListener) {
+            this.iRecyclerItemSelectedListener = iRecyclerItemSelectedListener;
+        }
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             txt_salon_address = itemView.findViewById(R.id.txt_salon_address);
             txt_salon_name = itemView.findViewById(R.id.txt_salon_name);
+            card_salon = itemView.findViewById(R.id.card_salon);
+            itemView.setOnClickListener(this);
 
+        }
+
+        @Override
+        public void onClick(View view) {
+            iRecyclerItemSelectedListener.OnItemSelectedListener(view,getAdapterPosition());
         }
     }
     @NonNull
@@ -48,6 +71,19 @@ public class MySalonAdapter extends RecyclerView.Adapter<MySalonAdapter.MyViewHo
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
         myViewHolder.txt_salon_name.setText(salonList.get(i).getName());
         myViewHolder.txt_salon_address.setText(salonList.get(i).getAddress());
+        if (!cardViewList.contains(myViewHolder.card_salon))
+        cardViewList.add(myViewHolder.card_salon);
+
+        myViewHolder.setiRecyclerItemSelectedListener(new IRecyclerItemSelectedListener() {
+            @Override
+            public void OnItemSelectedListener(View view, int pos) {
+                //set Background color for Unselected Items
+                for (CardView cardView:cardViewList)
+                    cardView.setCardBackgroundColor(context.getResources().getColor(android.R.color.white));
+
+                //Set BG Background color for selected Items
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/barbershop/Interface/IRecyclerItemSelectedListener.java
+++ app/src/main/java/com/example/barbershop/Interface/IRecyclerItemSelectedListener.java
@@ -1,4 +1,7 @@
 package com.example.barbershop.Interface;
 
+import android.view.View;
+
 public interface IRecyclerItemSelectedListener {
+    void OnItemSelectedListener(View view, int pos);
 }
