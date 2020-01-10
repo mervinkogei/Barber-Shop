f2011275800c7275ae56239001df741e9201ef8d
mervinkogei <vincent.kogei@yahoo.com>
2020-01-10 17:46:37 +0300

    Create a login Activity to authenticate users

--- app/src/main/java/com/example/barbershop/CreateAccountActivity.java
+++ app/src/main/java/com/example/barbershop/CreateAccountActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
